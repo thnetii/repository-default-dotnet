dfc2e4c713a59a8291ad6a81d2820b7eccab9eb3
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2021-04-20 11:20:27 +0200

    Add test directory

---
+++ test/THNETII.RepositoryDefault.Dotnet.Build.Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace THNETII.RepositoryDefault.Dotnet.Build.Test
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
